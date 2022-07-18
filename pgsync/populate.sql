c92575f99c66bdf09319037574bc042554385465
lephuhung <lephuhung77@gmail.com>
2022-07-18 22:27:10 +0700

    update work

--- pgsync/populate.sql
+++ pgsync/populate.sql
@@ -1,9 +1,33 @@
-CREATE TABLE IF NOT EXISTS product (
-    pk_id SERIAL NOT NULL,
-    name varchar(100) NOT NULL,
-    PRIMARY KEY ("pk_id")
+CREATE TABLE datadoc (
+	article_id  text  PRIMARY KEY,
+	topic text,
+	href text,
+	publish_date date,
+	newspaper text,
+	created_date date,
+	language text,
+	sapo text,
+	content text,
+	feature_image text
+);
+CREATE TABLE Page (
+  id_page text PRIMARY KEY,
+  name_page text,
+  link_page text
 );
 
-INSERT INTO product (name) VALUES ('test product1');
-INSERT INTO product (name) VALUES ('test product2');
-INSERT INTO product (name) VALUES ('test product3');
+CREATE TABLE Post_Page (
+  id_post_page SERIAL PRIMARY KEY,
+  id_page text,
+  id_post text,
+  publish_time date,
+  content text
+);
+
+CREATE TABLE Comment (
+  id_comment SERIAL PRIMARY KEY,
+  id_post_page int,
+  id_post text,
+  id_user text,
+  content text
+);
